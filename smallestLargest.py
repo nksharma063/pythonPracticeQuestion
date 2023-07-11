808f19c0a23b79490b6727f3b5df1ffbf3367493
Neeraj Sharma <nksharma063@gmail.com>
2023-07-11 17:35:17 +0530

    find the smallest and largest number in the list

---
+++ smallestLargest.py
@@ -0,0 +1,12 @@
+def smallestLargest(*args):
+    abc = sorted(list(args))
+    print(f"the smalles number is {abc[0]} and largest number is {abc[-1]}")
+
+smallestLargest(1,2,3,4,5)
+
+
+"""
+a = [1,2,3,4,5]
+print(min(a))
+print(max(a))
+"""
